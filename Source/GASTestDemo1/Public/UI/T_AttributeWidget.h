9e9d1ab9e4607525beed663d18cdedb441a7aaaa
BaoBaodesu <GitHub1034452732@qq.com>
2026-06-16 21:14:17 +0800

    first commit

---
+++ Source/GASTestDemo1/Public/UI/T_AttributeWidget.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "T_AttributeWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASTESTDEMO1_API UT_AttributeWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
